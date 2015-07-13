1107887ff25b21a5583f261d43f95f68843ee142
Paul Bowring <paul.bowring@ntlworld.com>
2015-07-13 22:47:18 +0100

    Heroku amendments

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/', function(req, res, next) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+  res.render('author', {title: 'Quiz'});
+});
+
 module.exports = router;
